a53fd174f7aac347f2ace20e7e5fc76651d32b3c
Isabela V <150187367+IsabelaValls14@users.noreply.github.com>
2025-04-28 13:34:35 -0600

    Agrega GET /items/:id endpoint con fetch test

--- API-Basico/app.js
+++ API-Basico/app.js
@@ -4,6 +4,12 @@ const path = require('path');
 
 app.use(express.static('public'));
 
+let catalogoItems = [];
+// let catalogoItems = [
+//   { id: 1, nombre: 'Espada', tipo: 'Arma', efecto: 'Daño alto' },
+//   { id: 2, nombre: 'Escudo', tipo: 'Defensa', efecto: 'Bloquea ataques' }
+// ];
+
 // Este endpoint sirve el archivo index.html cuando alguien visita "/"
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/html/index.html'));
@@ -22,3 +28,13 @@ app.get('/items', (req, res) => {
   
   res.json(catalogoItems);
 });
+
+app.get('/items/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const item = catalogoItems.find(i => i.id === id);
+
+  if (!item) {
+    return res.status(404).json({ mensaje: `Item con ID ${id} no encontrado` });
+  }
+  res.json(item)
+})
--- API-Basico/public/js/scripts.js
+++ API-Basico/public/js/scripts.js
@@ -5,3 +5,13 @@ fetch('/')
   .catch(error => {
     console.error('Error al hacer fetch a /', error);
   });
+
+  function testGetItemById(id) {
+  fetch(`/items/${id}`)
+    .then(response => response.json())
+    .then(data => console.log(`GET /items/${id} response:`, data))
+    .catch(error => console.error(`Error al hacer GET /items/${id}`, error));
+}
+
+// Llama la función para probar, por ejemplo buscando el ID 1:
+testGetItemById(1);
