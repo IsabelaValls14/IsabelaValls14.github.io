662194b7ff1c3c703c492c4a560add308660d59f
Isabela V <150187367+IsabelaValls14@users.noreply.github.com>
2025-04-28 12:13:16 -0600

    Serve index.html on / endpoint

--- API-Basico/app.js
+++ API-Basico/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Esto permite que Express sirva archivos de /public
 app.use(express.static('public'));
 
 // Este endpoint sirve el archivo index.html cuando alguien visita "/"
